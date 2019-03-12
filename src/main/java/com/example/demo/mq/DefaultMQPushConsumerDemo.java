615a1cdb27ddfe0c8bf5ca3efddf53bdd927eb3b
Yuwenbiao <ywb1996@189.cn>
2019-03-12 16:42:16 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/mq/DefaultMQPushConsumerDemo.java
@@ -0,0 +1,27 @@
+package com.example.demo.mq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+
+/**
+ * Consumer示例程序
+ *
+ * @author yuwb@corp.21cn.com
+ * @date 19-3-12 下午4:35
+ */
+public class DefaultMQPushConsumerDemo {
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please rename to unique group name");
+        consumer.setNamesrvAddr("192.168.249.47:9876");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        consumer.subscribe("TopicTest", "*");
+        consumer.registerMessageListener((MessageListenerConcurrently) (list, consumeConcurrentlyContext) -> {
+            System.out.printf(Thread.currentThread().getName() + "Receive New Messages:" + list + "%n");
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        consumer.start();
+    }
+}
---
+++ src/main/java/com/example/demo/mq/SyncProducer.java
@@ -0,0 +1,31 @@
+package com.example.demo.mq;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Producer示例程序
+ *
+ * @author yuwb@corp.21cn.com
+ * @date 19-3-12 下午4:16
+ */
+public class SyncProducer {
+    public static void main(String[] args) throws MQClientException, UnsupportedEncodingException, RemotingException, InterruptedException, MQBrokerException {
+        DefaultMQProducer producer = new DefaultMQProducer("Please_rename_unique_group_name");
+        producer.setNamesrvAddr("192.168.100.131:9876");
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            Message msg = new Message("Topic Test", "TagA", ("Hello RocketMQ" + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        producer.shutdown();
+    }
+}
